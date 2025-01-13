79146f173a75d6992bb028a37787e998d4df74ea
VilelaMarcus <marcusvilela000@gmail.com>
2025-01-12 23:29:17 -0300

    feat: add number in the cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { useCart } from "../context/CartContext";
 import "./Header.css";
 
 export default function Header() {
+  const { cartItemCount } = useCart();
+
   return (
     <header className="header">
       <h1>Totem de Pedidos</h1>
-      <FaShoppingCart size={24} className="cart-icon" />
+      <div className="cart-icon-container">
+        <FaShoppingCart size={50} className="cart-icon" />
+        {cartItemCount > 0 && <span className="cart-count">{cartItemCount}</span>}
+      </div>
     </header>
   );
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
+import { useCart } from "../context/CartContext";
 import "./ProductList.css";
 
 const products = {
@@ -24,20 +25,18 @@ const products = {
 const ProductList = () => {
   const { category } = useParams();
   const navigate = useNavigate();
+  const { addToCart } = useCart();
   const categoryProducts = products[category] || [];
 
   return (
     <div className="product-list">
-        <div style={{ display: "flex", flexDirection: "row" }}>
-            <button className="back-button" onClick={() => navigate("/")}>
-                <FaArrowLeft size={50} />
-            </button>
-            <h1>Produtos da categoria: {category}</h1>
-        </div>
-      
+      <button className="back-button" onClick={() => navigate("/")}>
+        <FaArrowLeft size={32} />
+      </button>
+      <h1>Produtos da categoria: {category}</h1>
       <div className="product-cards">
         {categoryProducts.map((product) => (
-          <div key={product.id} className="product-card">
+          <div key={product.id} className="product-card" onClick={() => addToCart(product)}>
             <img src={product.src} alt={product.name} />
             <h3>{product.name}</h3>
             <p>R${product.price}</p>
