e568c2dc652b6675527c5a4f44af82bfaed7d2d5
Rafael-Armond <rafaelarmond054@gmail.com>
2025-01-21 23:03:24 -0300

    recebendo um objeto de remessa de produtos do back

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,13 +2,30 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Carousel.css";
-import { useNavigate } from "react-router-dom"; // Assuming you are using react-router
+import { useNavigate } from "react-router-dom";
+import io from "socket.io-client";
+import { useEffect } from "react";
 
 const slides = [
-  { src: "../../public/assets/comida.jpg", title: "SALGADOS", category: "salgados" },
-  { src: "../../public/assets/bebida.jpg", title: "BEBIDAS", category: "bebidas" },
-  { src: "../../public/assets/doce.jpg", title: "DOCES", category: "doces" }
+  {
+    src: "../../public/assets/comida.jpg",
+    title: "SALGADOS",
+    category: "salgados",
+  },
+  {
+    src: "../../public/assets/bebida.jpg",
+    title: "BEBIDAS",
+    category: "bebidas",
+  },
+  { src: "../../public/assets/doce.jpg", title: "DOCES", category: "doces" },
 ];
+
+const socket = io("http://localhost:5000", {
+  transports: ["websocket"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 3000,
+});
 
 export default function Carousel() {
   const navigate = useNavigate();
@@ -24,6 +41,26 @@ export default function Carousel() {
   const handleCategoryDoubleClick = (category: string) => {
     navigate(`/produtos/${category}`);
   };
+
+  useEffect(() => {
+    if (!socket.connected) {
+      socket.connect();
+    }
+
+    socket.on("connect", () => {
+      console.log("Conectado ao WebSocket!");
+    });
+
+    socket.on("new_shipment", (shipment) => {
+      console.log("Remessa de salgados: ", shipment);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("new_shipment");
+      socket.disconnect();
+    };
+  });
 
   return (
     <div className="carousel">
